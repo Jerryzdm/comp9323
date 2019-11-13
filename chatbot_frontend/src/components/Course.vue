16241fc3a5946263888bba835a1da7bf21cb2c0f
DoryPaul <947478004@qq.com>
2019-11-13 15:07:51 +1100

    modify UI of course

--- chatbot_frontend/src/components/Course.vue
+++ chatbot_frontend/src/components/Course.vue
@@ -12,18 +12,32 @@
       <div style="clear: both"></div>
     </div>
     <a-list :grid="{ gutter: 16, column: 2 }" :dataSource="coursedata" :pagination="course_pagination"
-            style="margin-top: 10px;margin-right: 20px;margin-left: 20px">
+            style="margin: 10px 20px;">
       <a-list-item slot="renderItem" slot-scope="item, index">
         <a-card :title="item.courseCode" style="border-radius: 15px;">
 
           <!-- <p>{{item.courseCode}}</p>-->
           <div>
-          <a-icon type="environment" /><p>{{item.courseCampus}}</p>
+          <a-icon type="environment" style="float: left;"/><p style="float: left;margin-left: 20px">{{item.courseCampus}}</p>
+            <div style="clear: left"></div>
           </div>
-          <p>{{item.courseFaculty}}</p>
-          <p>{{item.courseTerms}}</p>
-          <p style="height: 70px">{{item.courseName}}</p>
-          <p>{{item.courseUOC}} UOC</p>
+          <div>
+          <a-icon type="cluster" style="float: left;"/><p style="float: left;margin-left: 20px">{{item.courseFaculty}}</p>
+            <div style="clear: left"></div>
+          </div>
+          <div>
+            <a-icon type="schedule" style="float: left;"/><p style="float: left;margin-left: 20px">{{item.courseTerms}}</p>
+          <div style="clear: left"></div>
+          </div>
+          <div>
+            <a-icon type="calculator" style="float: left;"/><p style="float: left;margin-left: 20px">{{item.courseUOC}} UOC</p>
+            <div style="clear: left"></div>
+          </div>
+          <div>
+          <a-icon type="read" style="float: left;"/>
+            <p style="float: left;margin-left: 20px;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;max-width: 250px; ">{{item.courseName}}</p>
+          <div style="clear: left"></div>
+  </div>
           <a slot="actions" :href="item.courseUrl" style="border-radius: 15px;">Details</a>
           <a slot="actions" @click="show_reviews(item.title)" style="border-radius: 15px;">Review</a>
           <a-switch slot="actions" checkedChildren="on" unCheckedChildren="off" :defaultChecked="false"/>
